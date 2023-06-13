f2a85a1a816b1025c37abd6dd73ea051d65be9da
Kestutis Januskevicius <infohata@gmail.com>
2023-06-13 14:27:13 +0200

    retrieve update destroy post view

--- ptu12_postit/postit_api/views.py
+++ ptu12_postit/postit_api/views.py
@@ -1,4 +1,6 @@
+from django.utils.translation import gettext_lazy as _
 from rest_framework import generics, permissions
+from rest_framework.exceptions import ValidationError
 from . import models, serializers
 
 
@@ -9,3 +11,29 @@ class PostList(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Post.objects.all()
+    serializer_class = serializers.PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def put(self, request, *args, **kwargs):
+        post = models.Post.objects.filter(
+            pk=kwargs['pk'], 
+            user=request.user
+        )
+        if post.exists():
+            return self.update(request, *args, **kwargs)
+        else:
+            raise ValidationError(_('You have no rights to update this.'))
+
+    def delete(self, request, *args, **kwargs):
+        post = models.Post.objects.filter(
+            pk=kwargs['pk'], 
+            user=request.user
+        )
+        if post.exists():
+            return self.destroy(request, *args, **kwargs)
+        else:
+            raise ValidationError(_('You have no rights to delete this.'))
