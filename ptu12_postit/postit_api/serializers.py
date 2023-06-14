b60c44ac8575354ea4fd594069a24cd0414f0cb0
Kestutis Januskevicius <infohata@gmail.com>
2023-06-14 09:21:31 +0200

    post comment serializer relationships

--- ptu12_postit/postit_api/serializers.py
+++ ptu12_postit/postit_api/serializers.py
@@ -15,7 +15,13 @@ class CommentSerializer(serializers.ModelSerializer):
 class PostSerializer(serializers.ModelSerializer):
     user = serializers.ReadOnlyField(source='user.username')
     user_id = serializers.ReadOnlyField(source='user.id')
+    comments = CommentSerializer(many=True, read_only=True)
+    # comments = serializers.StringRelatedField(many=True, read_only=True)
+    comment_count = serializers.SerializerMethodField()
+
+    def get_comment_count(self, obj):
+        return models.Comment.objects.filter(post=obj).count()
 
     class Meta:
         model = models.Post
-        fields = ['id', 'user', 'user_id', 'title', 'body', 'created_at']
+        fields = ['id', 'user', 'user_id', 'title', 'body', 'created_at', 'comment_count', 'comments']
